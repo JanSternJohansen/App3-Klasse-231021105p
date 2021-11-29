ca242ad755308ae0c98ab6f96fc4eccf74e6b96d
JanSternJohansen <jaj@tec.dk>
2021-11-29 10:10:23 +0100

    Commit2B

--- JansTest/nativescript.config.ts
+++ JansTest/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.JansFirst',
+  id: 'org.nativescript.JansTest',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
